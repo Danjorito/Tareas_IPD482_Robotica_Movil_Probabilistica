5a593543f2cdf51b0a21fd4a56d01ebb15eb1dca
Danjorito <daniel.gross@sansano.usm.cl>
2024-04-24 22:51:10 -0400

    Did some testing

--- Tarea_1/Ejercicio2.m
+++ Tarea_1/Ejercicio2.m
@@ -2,13 +2,15 @@ clc; clear;
 
 %Definicion de ruta
 step = 0.05;
-x = (0:step:10-step)';
-y = zeros(length(x), 1);
-theta_r = linspace(0, 8*pi, length(x))';
-
+N = 10/step;
+%x = linspace(0, 10, N);
+x = zeros(N, 1);
+y = zeros(N, 1);
+theta_r = linspace(0, 2*pi, length(x))';
+%theta_r = zeros(length(x), 1);
 
 %Parametros del robot
-T_k = 0.01;              %tiempo de sampling
+T_k = 0.0001;                %tiempo de sampling
 R = 0.45/2;                %radio del robot
 
 %Obtencion del perfil de velocidades
@@ -19,9 +21,9 @@ for k = linspace(1, length(x)-1, length(x)-1)
     theta_2 = theta_1 + 120*pi/180;
     theta_3 = theta_2 + 120*pi/180;
 
-    v(k+1, 1) = -sin(theta_1)*(x(k) - x(k+1))/T_k + cos(theta_1)*(y(k) - y(k+1))/T_k + R*(theta_r(k) - theta_r(k+1))/T_k;
-    v(k+1, 2) = -sin(theta_1+theta_2)*(x(k) - x(k+1))/T_k + cos(theta_1+theta_2)*(y(k) - y(k+1))/T_k + R*(theta_r(k) - theta_r(k+1))/T_k;
-    v(k+1, 3) = -sin(theta_1+theta_3)*(x(k) - x(k+1))/T_k + cos(theta_1+theta_3)*(y(k) - y(k+1))/T_k + R*(theta_r(k) - theta_r(k+1))/T_k;
+    v(k+1, 1) = -sin(theta_1)*-1*(x(k) - x(k+1))/T_k + cos(theta_1)*-1*(y(k) - y(k+1))/T_k + R*-1*(theta_r(k) - theta_r(k+1))/T_k;
+    v(k+1, 2) = -sin(theta_1+theta_2)*-1*(x(k) - x(k+1))/T_k + cos(theta_1+theta_2)*-1*(y(k) - y(k+1))/T_k + R*-1*(theta_r(k) - theta_r(k+1))/T_k;
+    v(k+1, 3) = -sin(theta_1+theta_3)*-1*(x(k) - x(k+1))/T_k + cos(theta_1+theta_3)*-1*(y(k) - y(k+1))/T_k + R*-1*(theta_r(k) - theta_r(k+1))/T_k;
 end
 
 x_p = zeros(length(x), 1);
@@ -33,9 +35,9 @@ for k = linspace(1, length(x)-1, length(x)-1)
     theta_2 = theta_1 + 120*pi/180;
     theta_3 = theta_2 + 120*pi/180;
 
-    x_p(k+1) = x_p(k)+T_k*(-sin(theta_1)*v(k, 1) -sin(theta_1+theta_2)*v(k, 2) -sin(theta_1+theta_3)*v(k, 3));
-    y_p(k+1) = y_p(k)+T_k*(cos(theta_1)*v(k, 1) +cos(theta_1+theta_2)*v(k, 2) +cos(theta_1+theta_3)*v(k, 3));
-    theta_r_p(k+1) = theta_r_p(k) + (T_k/(2*R))*(v(k, 1)+v(k, 2)+v(k, 3));
+    x_p(k+1) = x_p(k)+2/3*T_k*(-sin(theta_1)*v(k, 1) -sin(theta_1+theta_2)*v(k, 2) -sin(theta_1+theta_3)*v(k, 3));
+    y_p(k+1) = y_p(k)+2/3*T_k*(cos(theta_1)*v(k, 1) +cos(theta_1+theta_2)*v(k, 2) +cos(theta_1+theta_3)*v(k, 3));
+    theta_r_p(k+1) = theta_r_p(k) + (2/3*T_k/(2*R))*(v(k, 1)+v(k, 2)+v(k, 3));
 end
 
 disp('end');
