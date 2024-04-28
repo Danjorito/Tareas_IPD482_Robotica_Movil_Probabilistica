60964765892373a49d854c337507ae4382864e40
Daniel Gross <daniel.gross@sansano.usm.cl>
2024-04-28 03:24:04 -0400

    Wrote code for Problem 3 and wrote all of problem 3

--- Tarea_1/Ejercicio2.m
+++ Tarea_1/Ejercicio2.m
@@ -1,64 +1,3 @@
-%%
-%Definicion de ruta
-clc; clear;
-close all;
-
-step = 0.001;
-N = 10/step;
-x = linspace(0, 10, N)';
-%x = zeros(N, 1);
-%y = linspace(0, 10, N)';
-y = zeros(N, 1);
-%theta_r = zeros(N, 1);
-theta_r = linspace(0, 8*pi, N)';
-%theta_r(1:400) = linspace(0, pi, N/2-100)';
-%theta_r(401:800) = linspace(pi, 0, N/2-100)';
-%theta_r(1:100) = linspace(0, 5*pi/4, 100);
-%theta_r(101:1000) = 5*pi/4;
-
-%Parametros del robot
-T_k = 0.001;                %tiempo de sampling
-R = 0.45/2;                %radio del robot
-t = linspace(0, N*T_k, N);
-
-%Obtencion del perfil de velocidades
-v = zeros(N, 3); %Vector de velocidades v1, v2 y v3
-
-for k = linspace(2, N, N-1)
-    theta_1 = theta_r(k);
-
-    v(k, 1) = -sin(theta_1)*(x(k) - x(k-1))/T_k + cos(theta_1)*(y(k) - y(k-1))/T_k + R*(theta_r(k) - theta_r(k-1))/T_k;
-    v(k, 2) = -cos(theta_1+pi/6)*(x(k) - x(k-1))/T_k - sin(theta_1+pi/6)*(y(k) - y(k-1))/T_k + R*(theta_r(k) - theta_r(k-1))/T_k;
-    v(k, 3) = cos(theta_1-pi/6)*(x(k) - x(k-1))/T_k + sin(theta_1-pi/6)*(y(k) - y(k-1))/T_k + R*(theta_r(k) - theta_r(k-1))/T_k;
-end
-
-figure(1)
-hold on;
-plot(t, v);
-legend("v1", "v2", "v3");
-
-x_p = zeros(length(x), 1);
-y_p = zeros(length(x), 1);
-theta_r_p = zeros(length(x), 1);
-
-for k = linspace(1, length(x)-1, length(x)-1)
-    theta_1 = theta_r_p(k);
-    x_p(k+1) = x_p(k)+1/3*T_k*(-2*sin(theta_1)*v(k, 1) + (sin(theta_1)-sqrt(3)*cos(theta_1))*v(k, 2) + (sin(theta_1)+sqrt(3)*cos(theta_1))*v(k, 3));
-    y_p(k+1) = y_p(k)+1/3*T_k*(2*cos(theta_1)*v(k, 1) + (-sqrt(3)*sin(theta_1)-cos(theta_1))*v(k, 2) + (sqrt(3)*sin(theta_1)-cos(theta_1))*v(k, 3));
-    theta_r_p(k+1) = theta_r_p(k) + (1/3*T_k/(R))*(v(k, 1)+v(k, 2)+v(k, 3));
-end
-
-pause(1);
-for j = linspace(1, length(x)-1, length(x)-1)
-    figure(2)
-    quiver(x_p(j), y_p(j), 0.5*cos(theta_r_p(j)), 0.5*sin(theta_r_p(j)), "-or")
-    xlim([-10 10])
-    ylim([-10 10])
-    pause(T_k)
-end
-
-%%
-
 %Definicion de ruta
 clc; clear;
 close all;
@@ -66,15 +5,8 @@ close all;
 step = 0.01;
 N = 10/step;
 x = linspace(0, 10, N)';
-%x = zeros(N, 1);
-%y = linspace(0, 10, N)';
 y = zeros(N, 1);
-%theta_r = zeros(N, 1);
 theta_r = linspace(0, 8*pi, N)';
-%theta_r(1:400) = linspace(0, pi, N/2-100)';
-%theta_r(401:800) = linspace(pi, 0, N/2-100)';
-%theta_r(1:100) = linspace(0, 5*pi/4, 100);
-%theta_r(101:1000) = 5*pi/4;
 
 %Parametros del robot
 T_k = 0.001;                %tiempo de sampling
@@ -95,7 +27,10 @@ end
 figure(1)
 hold on;
 plot(t, v);
-legend("v1", "v2", "v3");
+legend("$v_{W1}$", "$v_{W2}$", "$v_{W3}$",'Interpreter','latex');
+title("Perfiles de velocidad lineal de las tres ruedas")
+xlabel("tiempo [s]");
+ylabel("velocidad [m/s]")
 
 x_p = zeros(length(x), 1);
 y_p = zeros(length(x), 1);
@@ -107,13 +42,23 @@ for k = linspace(1, length(x)-1, length(x)-1)
     x_p(k+1) = x_p(k) + 2*T_k/3*(cos(theta_1)*v(k, 1) + sin(theta_1-pi/6)*v(k, 2) - sin(theta_1+pi/6)*v(k, 3));
     y_p(k+1) = y_p(k) + 2*T_k/3*(sin(theta_1)*v(k, 1) - cos(theta_1-pi/6)*v(k, 2) + cos(theta_1+pi/6)*v(k, 3));
     theta_r_p(k+1) = theta_r_p(k) - (2*T_k)/(3*R)*(v(k, 1) + v(k, 2) + v(k, 3));
+    
 end
+
 
 pause(1);
 for j = linspace(1, length(x)-1, length(x)-1)
     figure(2)
-    quiver(x_p(j), y_p(j), 0.5*cos(theta_r_p(j)), 0.5*sin(theta_r_p(j)), "-or")
-    xlim([-10 10])
+    quiver(x_p(j), y_p(j), 0.6*0.5*cos(theta_r_p(j)), 0.5*sin(theta_r_p(j)), "-or")
+    xlim([-1 11])
     ylim([-10 10])
+    hold on;
+    line([0 10], [0 0], 'LineStyle',':', 'Color','blue');
+    plot(0, 0, 'go');
+    plot(10, 0, 'go');
+    text(-0.5, 1, "Origen");
+    text(9.5, 1, "Destino");
+
     pause(T_k)
+    hold off;
 end
---
+++ Tarea_1/Ejercicio3.m
@@ -0,0 +1,199 @@
+%Definicion de ruta (Caso ruta circular)
+clc; clear;
+close all;
+
+step = 0.01;
+N = 10/step;
+
+angle = linspace(0, 2*pi, N);
+r = 5;
+x = r*cos(angle);
+y = r*sin(angle);
+theta_r = angle + pi/2;
+
+
+%Parametros del robot
+T_k = 0.001;                %tiempo de sampling
+d = 0.555;
+t = linspace(0, N*T_k, N);
+
+%Obtencion del perfil de velocidades
+v_r = zeros(N, 1); %Vector de velocidad rueda derecha
+v_l = zeros(N, 1); %Vector de velocidad rueda izquierda
+
+for k = linspace(2, N, N-1)
+    theta1 = theta_r(k);
+
+    v_l(k-1) = cos(theta1)*(x(k)-x(k-1))/T_k + sin(theta1)*(y(k)-y(k-1))/T_k - d/2*(theta_r(k) - theta_r(k-1))/T_k;
+    v_r(k-1) = cos(theta1)*(x(k)-x(k-1))/T_k + sin(theta1)*(y(k)-y(k-1))/T_k + d/2*(theta_r(k) - theta_r(k-1))/T_k;
+end
+
+figure(1)
+hold on;
+plot(t, [v_l v_r]);
+legend("$v_{l}$", "$v_{r}$",'Interpreter','latex');
+title("Perfiles de velocidad lineal de las dos ruedas")
+xlabel("tiempo [s]");
+ylabel("velocidad [m/s]")
+
+
+%Estimación de posicion del robot a partir de los perfiles de velocidad obtenidos
+
+x_p_r = zeros(N, 1);
+x_p_r(1) = 5;
+y_p_r = zeros(N, 1);
+theta_p_r = zeros(N, 1);
+theta_p_r(1) = theta_r(1);     %Pose inicial del robot
+
+for k = linspace(1, N-1, N-1)
+    theta1 = theta_p_r(k);
+
+    x_p_r(k+1) = x_p_r(k) + T_k/2*cos(theta1)*(v_r(k)+v_l(k));
+    y_p_r(k+1) = y_p_r(k) + T_k/2*sin(theta1)*(v_r(k)+v_l(k));
+    theta_p_r(k+1) = theta_p_r(k) + T_k/d*(v_r(k)-v_l(k));
+end
+
+%Estimación de posicion del trailer a partir de los perfiles de velocidad obtenidos
+
+x_p_t = zeros(N, 1);
+x_p_t(1) = x_p_r(1) - 2*cos(theta_p_r(1));
+y_p_t = zeros(N, 1);
+y_p_t(1) = y_p_r(1) - 2*sin(theta_p_r(1));
+theta_p_t = zeros(N, 1);
+theta_p_t(1) = theta_r(1);     %Pose inicial del trailer
+
+for k = linspace(1, N-1, N-1)
+    theta1 = theta_p_t(k);
+    theta2 = theta_p_r(k);
+    beta = theta2 - theta1;
+
+    x_p_t(k+1) = x_p_t(k) + T_k*cos(theta1)*(sin(beta)*(v_r(k)-v_l(k))/d + 1/2*cos(beta)*(v_r(k)+v_l(k)));
+    y_p_t(k+1) = y_p_t(k) + T_k*sin(theta1)*(sin(beta)*(v_r(k)-v_l(k))/d + 1/2*cos(beta)*(v_r(k)+v_l(k)));
+    theta_p_t(k+1) = theta_p_t(k) + T_k*(-cos(beta)*(v_r(k)-v_l(k))/d + 1/2*sin(beta)*(v_r(k)+v_l(k)));
+end
+
+pause(1);
+for j = linspace(1, length(x)-1, length(x)-1)
+    figure(2)
+    quiver(x_p_r(j), y_p_r(j), 0.6*0.5*cos(theta_p_r(j)), 0.5*sin(theta_p_r(j)), "-or")
+    text(x_p_r(j), y_p_r(j)+1, "Robot");
+    hold on;
+    quiver(x_p_t(j), y_p_t(j), 0.6*0.5*cos(theta_p_t(j)), 0.5*sin(theta_p_t(j)), "-or")
+    text(x_p_t(j), y_p_t(j)+1, "Trailer");
+    xlim([-10 10])
+    ylim([-10 10])
+    plot(x, y, 'LineStyle',':', 'Color','blue');
+    plot(5, 0, 'go');
+    text(5.5, 0, "Origen/Destino");
+    hold off;
+    pause(T_k)
+end
+
+%%
+
+angle = linspace(0, 2*pi, N);
+r = 5;
+x = r*cos(angle);
+y = r*sin(angle);
+theta_r = angle + pi/2;
+
+
+figure(123)
+xlim([-10 10])
+ylim([-10 10])
+hold on;
+plot(x, y, 'LineStyle',':', 'Color','blue');
+plot(5, 0, 'go');
+text(5.5, 0, "Origen/Destino");
+title("Ruta descrita para el robot y el tráiler")
+xlabel("$X_I$ [m]",'Interpreter','latex')
+ylabel("$Y_I$ [m]",'Interpreter','latex')
+
+%%
+%Definicion de ruta (Ruta recta)
+clc; clear;
+close all;
+
+step = 0.01;
+N = 10/step;
+
+x = linspace(0, 10, N)';
+y = zeros(N, 1);
+theta_r = zeros(N, 1);
+
+%Parametros del robot
+T_k = 0.001;                %tiempo de sampling
+d = 0.555;
+t = linspace(0, N*T_k, N);
+
+%Obtencion del perfil de velocidades
+v_r = zeros(N, 1); %Vector de velocidad rueda derecha
+v_l = zeros(N, 1); %Vector de velocidad rueda izquierda
+
+for k = linspace(2, N, N-1)
+    theta1 = theta_r(k);
+
+    v_l(k-1) = cos(theta1)*(x(k)-x(k-1))/T_k + sin(theta1)*(y(k)-y(k-1))/T_k - d/2*(theta_r(k) - theta_r(k-1))/T_k;
+    v_r(k-1) = cos(theta1)*(x(k)-x(k-1))/T_k + sin(theta1)*(y(k)-y(k-1))/T_k + d/2*(theta_r(k) - theta_r(k-1))/T_k;
+end
+
+figure(1)
+hold on;
+plot(t, [v_l v_r]);
+legend("$v_{l}$", "$v_{r}$",'Interpreter','latex');
+title("Perfiles de velocidad lineal de las dos ruedas")
+xlabel("tiempo [s]");
+ylabel("velocidad [m/s]")
+
+
+%Estimación de posicion del robot a partir de los perfiles de velocidad obtenidos
+
+x_p_r = zeros(N, 1);
+y_p_r = zeros(N, 1);
+theta_p_r = zeros(N, 1);
+theta_p_r(1) = theta_r(1);     %Pose inicial del robot
+
+for k = linspace(1, N-1, N-1)
+    theta1 = theta_p_r(k);
+
+    x_p_r(k+1) = x_p_r(k) + T_k/2*cos(theta1)*(v_r(k)+v_l(k));
+    y_p_r(k+1) = y_p_r(k) + T_k/2*sin(theta1)*(v_r(k)+v_l(k));
+    theta_p_r(k+1) = theta_p_r(k) + T_k/d*(v_r(k)-v_l(k));
+end
+
+%Estimación de posicion del trailer a partir de los perfiles de velocidad obtenidos
+
+x_p_t = zeros(N, 1);
+x_p_t(1) = x_p_r(1) - 2*cos(theta_p_r(1));
+y_p_t = zeros(N, 1);
+y_p_t(1) = y_p_r(1) - 2*sin(theta_p_r(1));
+theta_p_t = zeros(N, 1);
+theta_p_t(1) = theta_r(1);     %Pose inicial del trailer
+
+for k = linspace(1, N-1, N-1)
+    theta1 = theta_p_t(k);
+    theta2 = theta_p_r(k);
+    beta = theta2 - theta1;
+
+    x_p_t(k+1) = x_p_t(k) + T_k*cos(theta1)*(sin(beta)*(v_r(k)-v_l(k))/d + 1/2*cos(beta)*(v_r(k)+v_l(k)));
+    y_p_t(k+1) = y_p_t(k) + T_k*sin(theta1)*(1.8*sin(beta)*(v_r(k)-v_l(k))/d + 1/2*cos(beta)*(v_r(k)+v_l(k)));
+    theta_p_t(k+1) = theta_p_t(k) + T_k*(-cos(beta)*(v_r(k)-v_l(k))/d + 1/2*sin(beta)*(v_r(k)+v_l(k)));
+end
+
+for j = linspace(1, length(x)-1, length(x)-1)
+    figure(2)
+    quiver(x_p_r(j), y_p_r(j), 0.6*0.5*cos(theta_p_r(j)), 0.5*sin(theta_p_r(j)), "-or")
+    text(x_p_r(j), y_p_r(j)-0.1, "Robot");
+    hold on;
+    quiver(x_p_t(j), y_p_t(j), 0.6*0.5*cos(theta_p_t(j)), 0.5*sin(theta_p_t(j)), "-or")
+    text(x_p_t(j), y_p_t(j)-0.1, "Trailer");
+    xlim([-2 11])
+    ylim([-1 1])
+    line([0 10], [0 0], 'LineStyle',':', 'Color','blue');
+    plot(0, 0, 'go');
+    plot(10, 0, 'go');
+    text(-0.5, 0.1, "Origen");
+    text(9.5, 0.1, "Destino");
+    hold off;
+    pause(T_k)
+end
